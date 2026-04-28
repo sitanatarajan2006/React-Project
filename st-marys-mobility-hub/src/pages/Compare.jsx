6072a4dae502db839a6e7e63190c494f3a2ba18a
sitanatarajan2006 <sitanatarajan3@gmail.com>
2026-04-28 19:20:13 +0100

    addedcomparing travel methods page with basic filler

--- st-marys-mobility-hub/src/pages/Compare.jsx
+++ st-marys-mobility-hub/src/pages/Compare.jsx
@@ -1,4 +1,14 @@
 function Compare() {
-  return <h2>Compare Travel Modes</h2>;
+  return (
+    <div>
+      <h2>Compare Travel Modes</h2>
+
+      <p>
+        Compare walking, cycling, bus and rail using estimated cost, journey time,
+        convenience and carbon impact.
+      </p>
+    </div>
+  );
 }
+
 export default Compare;
