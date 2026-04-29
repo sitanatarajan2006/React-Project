edaf4949368bafdf3e5829f82a0519e24552123f
sitanatarajan2006 <sitanatarajan3@gmail.com>
2026-04-29 20:48:24 +0100

    added info to home page

--- st-marys-mobility-hub/src/pages/Home.jsx
+++ st-marys-mobility-hub/src/pages/Home.jsx
@@ -1,4 +1,27 @@
 function Home() {
-  return <h2>Home  Page</h2>;
+  return (
+    <div>
+      <h1>St Mary’s Urban Mobility Hub</h1>
+
+      <p>
+        This application provides guidance on urban travel and helps users plan
+        journeys using different transport options such as bus, rail, cycling,
+        and walking.
+      </p>
+
+      <p>
+        Users can compare travel modes, estimate costs, and explore suitable
+        journey options based on their needs.
+      </p>
+
+      <h3>How to use this application:</h3>
+      <ul>
+        <li>Use the Journey Planner to explore travel routes</li>
+        <li>Compare travel modes to choose the best option</li>
+        <li>Estimate journey costs using the cost calculator</li>
+      </ul>
+    </div>
+  );
 }
+
 export default Home;
